d49f4c77e53277fe33c69773f5aa20eb0c0acbbf
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-28 00:42:37 +0300

    Ask for input from the user on the command line.

---
+++ python_tutorial/materials/cmd_input.py
@@ -0,0 +1,5 @@
+# To ask for input from the user on the command line, we use the input() function.
+# Note that input() returns an object that is a string.
+
+input_name = input("What's your name? ")
+print("Hello,", input_name, "!")
