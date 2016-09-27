9796a43b31cd438a0c2d75745b687279ae7eb597
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-27 23:38:22 +0300

    Add better comments - exercise explanations.

--- python_tutorial/exercises/exercise_1.py
+++ python_tutorial/exercises/exercise_1.py
@@ -1,3 +1,15 @@
+# Write a program that produces the following output:
+# 
+# 000000001
+# 000000022
+# 000000333
+# 000004444
+# 000055555
+# 000666666
+# 007777777
+# 088888888
+# 999999999
+
 # Iterate through each line.
 for num in range (1, 10):
     # Iterate from the beginning and place 0s.
--- python_tutorial/exercises/exercise_2.py
+++ python_tutorial/exercises/exercise_2.py
@@ -1,6 +1,12 @@
+# Write a program that asks the user to provide different numbers of banknotes and coins, from €50 to €1. 
+# Then output the total sum in euros.
+
+# Initialize the sum and assign 0 to it.
 sum = 0;
 
+# Ask user for different numbers of banknotes and coins.
 user_input = input("Enter number of 50 euro banknotes: ")
+# Increase the sum.
 sum += int(user_input) * 50
 user_input = input("Enter number of 20 euro banknotes: ")
 sum += int(user_input) * 20
@@ -13,4 +19,5 @@ sum += int(user_input) * 2
 user_input = input("Enter number of 1 euro coins: ")
 sum += int(user_input) * 1
 
+# Print the sum.
 print("You have", sum, "euros")
