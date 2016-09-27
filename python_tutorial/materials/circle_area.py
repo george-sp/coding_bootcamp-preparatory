82623ad3a2813f1927b2615112af20564fc6a471
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-28 00:48:35 +0300

    Calculate the circle area given the circle radius.

---
+++ python_tutorial/materials/circle_area.py
@@ -0,0 +1,11 @@
+# Ask for the circle radius from the user.
+input_radius = input("Give the circle radius (must be integer): ")
+
+# Convert user's input to an integer.
+input_radius = int(input_radius)
+
+# Calculate the circle area.
+area = input_radius**2 * 3.14
+
+# Print the circle area out.
+print(area)
