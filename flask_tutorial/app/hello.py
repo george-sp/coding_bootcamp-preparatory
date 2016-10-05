962d0ae0c8c1135242a0a2292333109f678f8b57
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-05 15:02:28 +0300

    Show the post with the given id.

--- flask_tutorial/app/hello.py
+++ flask_tutorial/app/hello.py
@@ -10,5 +10,10 @@ def hello_world():
 def hello_name(name):
     return 'Hello %s!' % name
 
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
 if __name__ == '__main__':
    app.run(debug = True)
