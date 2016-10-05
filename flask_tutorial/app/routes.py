9a6d1c7c30fccaf8b6f6f1fc43d8c551fb29a243
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-06 00:32:05 +0300

    Flash a message.

--- flask_tutorial/app/routes.py
+++ flask_tutorial/app/routes.py
@@ -1,10 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, url_for, flash, send_from_directory, request, redirect, render_template
 from forms import ContactForm
 
 app = Flask(__name__)
 
+app.secret_key = 'random string'
+
 @app.route('/new', methods = ['GET', 'POST'])
 def new():
     if (request.method == 'POST'):
-        pass
+        if (not request.form['name'] or not request.form['surname'] \
+                or not request.form['email'] or not request.form['password']):
+            flash('Please fill all the fields.', 'error')
+        else:
+            return redirect(url_for('index'))
     return render_template('new.html')
