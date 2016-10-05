8d07fb908981dc4f76abd00824f3503f98d3e6c4
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-05 14:59:05 +0300

    Add variable parts to a URL.

--- flask_tutorial/app/hello.py
+++ flask_tutorial/app/hello.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s!' % name
+
 if __name__ == '__main__':
    app.run(debug = True)
