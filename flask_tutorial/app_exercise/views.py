09bd6e94533cd2b12a54da8c6a8937064eabf6b6
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-06 13:59:40 +0300

    Complete Exercise 1.3 - flash message.

--- flask_tutorial/app_exercise/views.py
+++ flask_tutorial/app_exercise/views.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from form import SignUpForm
 
 app = Flask(__name__)
 
-@app.route('/new', methods=['GET', 'POST'])
+app.secret_key = 'random string'
+
+@app.route('/new')
 def new():
+    return render_template('new.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Check if the customer has filled all the fields
+        if (not request.form['name'] or not request.form['surname'] \
+                or not request.form['email'] or not request.form['password']):
+            flash('All the fields are required to sign up.', 'error')
+        # If there are errors, open the new.html
     return render_template('new.html')
 
 if __name__ == '__main__':
