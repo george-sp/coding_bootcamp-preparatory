0e9c7c4f34466895b56d5e6f7415edcbdd1f298b
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-06 09:45:11 +0300

    Add routes for the login and logout.

--- flask_tutorial/app/routes.py
+++ flask_tutorial/app/routes.py
@@ -16,3 +16,18 @@ def new():
             session['name'] = request.form['name']
             return login()
     return render_template('new.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if 'name' in session:
+        name = session['name']
+        return 'You are logged in as ' + name + '<br>' + \
+         "<b><a href = '/logout'>click here to log out</a></b>"
+    return "You are not logged in <br><a href = '/login'></b>" + \
+      "click here to log in</b></a>"
+
+@app.route('/logout')
+def logout():
+   # remove the name from the session if it is there
+   session.pop('name', None)
+   return redirect(url_for('index'))
