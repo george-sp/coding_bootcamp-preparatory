45c0592c09f0637fd599c63831697727f9ebdaba
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-11 01:37:43 +0300

    Hotfix remove the counter variable. Remove it from adjacent cells.

--- maze_challenge/maze_challenge.py
+++ maze_challenge/maze_challenge.py
@@ -102,10 +102,10 @@ for coord in list_coords:
     # Create a list of the four adjacent cells
     # with a counter variable of the current element's counter variable +1.
     adjacent_coords = [
-    [coord[0] - 1, coord[1], step_counter], # top adjacent
-    [coord[0], coord[1] - 1, step_counter], # left adjacent
-    [coord[0] + 1, coord[1], step_counter], # bottom adjacent
-    [coord[0], coord[1] + 1, step_counter]  # right adjacent
+    [coord[0] - 1, coord[1]], # top adjacent
+    [coord[0], coord[1] - 1], # left adjacent
+    [coord[0] + 1, coord[1]], # bottom adjacent
+    [coord[0], coord[1] + 1]  # right adjacent
     ]
 
     if (debug):
