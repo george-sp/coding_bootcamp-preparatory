5bd51fa367a795fe02edea9adab0c9cbdffe2e76
George Spyridakis <spiridakis.grg@gmail.com>
2017-06-10 15:09:04 +0300

    Find all maze solutions recursively

--- maze_challenge/maze_challenge.py
+++ maze_challenge/maze_challenge.py
@@ -83,6 +83,71 @@ def getMazeBlocks(height, width):
                 block_points.append(maze_coords[r][c])
     return block_points
 
+"""
+Call this method recursively and search for the exit path.
+"""
+def searchNeighboringCoords(coord, coords_path):
+	if (debug): print("Current cell:", coord)
+	if (debug): print("Current path:", coords_path)
+
+	# Add the current coord to the path
+	cells_path = list(coords_path)
+	cells_path.append(coord)
+
+	if (coord[0] == maze_goal[0] and coord[1] == maze_goal[1]):
+		print("========================================\nSolution Found: %s\n========================================\n" % cells_path)
+		list_coords.append(cells_path)
+
+	if (debug): print("Blocks: ", maze_blocks)
+
+    # Create a list of the four adjacent cells
+    # with a counter variable of the current element's counter variable +1.
+	adjacent_coords = [
+	[coord[0] - 1, coord[1]], # top adjacent
+	[coord[0], coord[1] - 1], # left adjacent
+	[coord[0] + 1, coord[1]], # bottom adjacent
+	[coord[0], coord[1] + 1]  # right adjacent
+	]
+
+	if (debug): print("Adjacent cells:", adjacent_coords)
+
+    # Use a copy the list so that we can iterate and modify at the same time.
+	adjacent_coords_copy = adjacent_coords.copy()
+	for cell in adjacent_coords_copy:
+        # Check if the cell is out of the border.
+		if (cell[0] > maze_rows_len or
+			cell[0] <= 0 or 
+			cell[1] > maze_cols_len or
+			cell[1] <= 0):
+			if (debug): print("Remove cell: %s, as Out of Border" % cell)
+			adjacent_coords.remove(cell)
+        # Check if the cell is a block.
+		elif (cell in maze_blocks):
+			if (debug): print("Remove cell: %s as in Blocks" % cell)
+			adjacent_coords.remove(cell)
+		# Check if the cell is already in the current path
+		else:
+			for cell_in_path in coords_path:
+				# Remove the step_counter so we can compare them
+				cell_in_path = cell_in_path[:-1]
+				if cell_in_path == cell:
+					if (debug): print("Remove cell: %s, as Already in Path" % cell)
+					adjacent_coords.remove(cell)
+
+	if not adjacent_coords:
+		if (debug): print("====================> Not any available adjacent cells\n")
+		return None
+	else:
+		if (debug): print("Adjacent cells available:", adjacent_coords)
+		for cell in adjacent_coords:
+			cell.append(coord[2] + 1)
+
+			if (debug): print("searchNeighboringCoord(%s, %s\n" % (cell, cells_path))
+			searchNeighboringCoords(cell, cells_path)
+
+"""
+Run the game
+"""
 
 maze = getMaze().split("\n")
 maze_rows_len, maze_cols_len = getMazeDimensions(maze)
@@ -91,40 +156,14 @@ maze_coords = getMazeCoordinates(height=maze_rows_len, width=maze_cols_len)
 maze_start, maze_goal = getMazeStartEnd(height=maze_rows_len, width=maze_cols_len)
 maze_blocks = getMazeBlocks(height=maze_rows_len, width=maze_cols_len)
 
-# List of coordinates which will be used as a queue.
-# The queue will be initialized with one coordinate, the end coordinate.
-# Each coordinate will also have a counter variable attached.
 step_counter = 0
-maze_goal.append(step_counter)
-list_coords = [maze_goal]
-# Go through every element in the queue.
-for coord in list_coords:
-    # Create a list of the four adjacent cells
-    # with a counter variable of the current element's counter variable +1.
-    adjacent_coords = [
-    [coord[0] - 1, coord[1]], # top adjacent
-    [coord[0], coord[1] - 1], # left adjacent
-    [coord[0] + 1, coord[1]], # bottom adjacent
-    [coord[0], coord[1] + 1]  # right adjacent
-    ]
+maze_start.append(step_counter)
+list_coords = []
 
-    # Use a copy the list so that we can iterate and modify at the same time.
-    adjacent_coords_copy = adjacent_coords.copy()
-    for cell in adjacent_coords_copy:
-        # Check if the cell is out of the border.
-        if (cell[0] > maze_rows_len or cell[1] > maze_cols_len):
-            adjacent_coords.remove(cell)
-        # Check if the cell is a block.
-        elif (cell in maze_blocks):
-            adjacent_coords.remove(cell)
-
-
-    if (debug):
-        print("Adjacent cells:", adjacent_coords)
-
+searchNeighboringCoords(maze_start, [])
 
 if (debug):
-    print("  Debugging  \n-------------")
+    print("--------------------------\n\tDebugging\n--------------------------")
     print("Number of rows:", maze_rows_len)
     print("Number of columns:", maze_cols_len)
     print("Start:", maze_start)
@@ -141,4 +180,6 @@ if (debug):
     for row in maze_coords:
         print(row)
     print("List of coordinates:")
-    print(list_coords)
+    for list_num in range (len(list_coords)):
+    	print("Solution %d: %s" % (list_num,list_coords[list_num]))
+    print("Total solutions found: %d" % len(list_coords))
